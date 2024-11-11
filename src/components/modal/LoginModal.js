715a5d0c09160521a2581961f4a16c7a60ff35fa
RobertDeveloperBlog <robert.askarov43@gmail.com>
2024-11-12 01:05:25 +0300

    login-added

--- src/components/modal/LoginModal.js
+++ src/components/modal/LoginModal.js
@@ -1,14 +1,20 @@
 import { useDisclosure } from '@mantine/hooks';
 import { Modal, Button, Group } from '@mantine/core';
 import LoginButton from "../button/LoginButton";
+import ModalContent from "./modalContent/ModalContent";
 
 export default function LoginModal() {
     const [opened, { open, close }] = useDisclosure(false);
 
     return (
         <>
-            <Modal opened={opened} onClose={close} title="Log in">
-
+            <Modal opened={opened} onClose={close}
+                   title="Авторизация:" centered={false}
+                   size='md' overlayProps={{
+                opacity: 0.55,
+                blur: 1,
+            }}>
+            <ModalContent/>
             </Modal>
 
             <Group position="center">
