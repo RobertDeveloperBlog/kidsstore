6fa75ddf4d51a6634808396526031ea3586695e1
robert <robert.askarov43@gmail.com>
2024-11-17 23:24:37 +0300

    fixedCatalogCard

--- src/components/pages/catalogPage/catalogCard/CatalogCard.js
+++ src/components/pages/catalogPage/catalogCard/CatalogCard.js
@@ -2,18 +2,15 @@ import React, { useState } from 'react';
 import {Button} from "@mantine/core";
 import './CatalogCard.css'
 
+
+
 const CatalogCard = () => {
 
-    const [isChanged, setIsChanged] = useState(false);
-    const [value, setValue] = useState(0); 
-
-    function handleButtonClick() {
-        setIsChanged(!isChanged)   
-    }
+    const [value, setValue] = useState(0);
 
     const handleDecrement = () => {
         setValue(value - 1)
-        if (value == 0) {
+        if (value === 0) {
             setValue(0)
         }
     }
@@ -34,12 +31,20 @@ const CatalogCard = () => {
             <div className="card-price-container">
                 <div className="card-price">1499 ₽</div>
             </div>
-            <Button className="card-btn" color="dark" onClick={handleButtonClick}>{isChanged ? 'Добавлено' : 'В корзину'}</Button>
-            <div className="card-btn-changed">
-                <Button className="card-btn-changed-minus" onClick={handleDecrement}>-</Button>
-                <div>{value}</div>
-                <Button className="card-btn-changed-plus" onClick={handleIncrement}>+</Button>
-            </div>
+
+            {!Boolean(value) && <Button className="card-btn" color="dark" onClick={handleIncrement}>В корзину</Button>}
+
+            {Boolean(value) && (
+                <>
+                    <Button disabled={true} className="card-btn" color="dark"
+                            onClick={handleIncrement}>Добавлено</Button>
+                    <div className="card-btn-changed">
+                        <Button className="card-btn-changed-minus" onClick={handleDecrement}>-</Button>
+                        <div>{value}</div>
+                        <Button className="card-btn-changed-plus" onClick={handleIncrement}>+</Button>
+                    </div>
+                </>)
+            }
         </div>
     );
 };
